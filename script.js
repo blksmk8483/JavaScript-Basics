4c9ddefcd513f733004d7ea693eff5e94fdcba8d
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-18 12:29:53 -0500

    initial save for JavaScript basics

---
+++ script.js
@@ -0,0 +1,71 @@
+// let js = "amazing";
+// // console.log(40 + 8 + 23 - 10);
+
+// console.log("Hey, it's me! Brandon");
+
+// let firstName = "Brandon";
+
+// let lastName = "Knight";
+
+// console.log(firstName);
+
+// console.log(firstName, lastName);
+// console.log(`first name: ${firstName}, last name: ${lastName}`);
+
+// let myFirstJob = "concession stand worker";
+// let myCurrentJob = "Teacher";
+
+// console.log(`My first job was a ${myFirstJob} at a baseball field.`);
+
+// console.log(false);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Brandon");
+
+// javaScriptIsFun = "YES!";
+// console.log(javaScriptIsFun);
+
+// let year;
+// console.log(year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+let age = 39;
+age = 38;
+console.log(age);
+
+const birthYear = 1984;
+// birthYear = 1990;
+
+// const job
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+// // -----------------------------------------------------------------------------------
+
+// // LECTURE: Values and Variables ------------------------
+// let country = "United States";
+// let continent = "North America";
+// let population = "330,000,000 million";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// // LECTURE: Data Types ------------------------
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
