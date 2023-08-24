036c6d3c00ce3e2c5527fab56d219ec166c5ac02
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-24 16:09:40 -0500

    added strict mode

---
+++ script2.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hadDriversLicense = false;
+const passTest = true;
+
+if (passTest) hadDriversLicense = true;
+if (hadDriversLicense) console.log("I can drive");
+
+// const interface = "Audio";
+// const private = 123;
