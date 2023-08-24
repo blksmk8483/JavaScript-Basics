b729dac35c7db801f6e375b75cf3a54a4060e977
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-23 22:15:50 -0500

    Challenge #4 solved

--- script.js
+++ script.js
@@ -379,6 +379,17 @@ console.log(drink);
 
 console.log(`I like to drink ${age >= 18 ? "Dr. Pepper 🥤" : "water 🥛"}`);
 
+//  Challenge #4
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = bill <= 300 && bill >= 50 ? 0.15 * bill : 0.2 * bill;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
+
 // ------------   COURSE CHALLENGES   ------------
 // -----------------------------------------------------------------------------------
 
