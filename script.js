01a598d36c44ce5d3dfc187aab9ae52efe3b7e65
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-23 19:19:28 -0500

    another challenge down clear

--- script.js
+++ script.js
@@ -326,6 +326,30 @@ if (day === "monday") {
   console.log("Not a valid day!");
 }
 
+const language = "spanish";
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    s;
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too 😄");
+}
+
 // ------------   COURSE CHALLENGES   ------------
 // -----------------------------------------------------------------------------------
 
