099653466884e7f717afe475dd9b6a2d0929013b
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-24 18:55:29 -0500

    functions

--- script2.js
+++ script2.js
@@ -1,10 +1,28 @@
 "use strict";
 
-let hadDriversLicense = false;
-const passTest = true;
-
-if (passTest) hadDriversLicense = true;
-if (hadDriversLicense) console.log("I can drive");
-
 // const interface = "Audio";
 // const private = 123;
+
+// ----------------------------------------------------------------------------------
+// FUNCTIONS:
+
+function logger(a) {
+  console.log(`My name is Brandon ${a}`);
+}
+
+// calling / running / invoking function
+logger("does this work?");
+logger("HEY HEY, it does!!!");
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(3, 4));
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
