0e5c6ea5822f4b273d63a15e659823db08f91322
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-24 21:06:09 -0500

    function declarations vs. expressions and arrow functions

--- script2.js
+++ script2.js
@@ -26,3 +26,50 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+// ----------------------------------------------------------------------------------
+// FUNCTION DECLARATIONS vs. EXPRESSIONS:
+
+// function calcAge1(birthYear) {
+//   const age = 2037 - birthYear;
+//   return age;
+// }
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1984);
+console.log(age1);
+
+// function expression
+
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// arrow function
+const calcAge2 = (birthYear) => {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1984);
+
+console.log(age1, age2);
+
+// ----------------------------------------------------------------------------------
+// ARROW FUNCTIONS:
+
+// const yearsUntilRetirement = (birthYear) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return retirement;
+// };
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2023 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years.`;
+};
+
+console.log(yearsUntilRetirement(1984, "Brandon"));
+console.log(yearsUntilRetirement(1975, "Sarah"));
